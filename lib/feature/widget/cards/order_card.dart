e698d3de27779976a3ab31e9bd93654be1290ea3
Usmonaliyev <uzayyub2007@gmail.com>
2024-01-13 20:55:46 +0500

    selectrbleColor upgrade go

--- lib/feature/widget/cards/order_card.dart
+++ lib/feature/widget/cards/order_card.dart
@@ -14,43 +14,81 @@ class OrderCard extends StatelessWidget {
           color: const Color(0xFFEAEBED),
         ),
       ),
-      child: const Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Address",
-            style: AppFonts.hh3Bold,
-          ),
-          Row(
-            children: [
-              Text("Price", style: AppFonts.bb2SemiBold),
-              Spacer(),
-              Text("\$87.10", style: AppFonts.bb2SemiBold),
-            ],
-          ),
-          Row(
-            children: [
-              Text("Squid Sweet and Sour Salad",
-                  style: AppFonts.captionRegular),
-              Spacer(),
-              Text("\$87.10", style: AppFonts.captionRegular),
-            ],
-          ),
-          Row(
-            children: [
-              Text("Japan Hainanese Sashimi", style: AppFonts.captionRegular),
-              Spacer(),
-              Text("\$87.10", style: AppFonts.captionRegular),
-            ],
-          ),
-          Row(
-            children: [
-              Text("Black Pepper Beef Lumpia", style: AppFonts.captionRegular),
-              Spacer(),
-              Text("\$87.10", style: AppFonts.captionRegular),
-            ],
-          ),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Address",
+              style: AppFonts.hh3Bold,
+            ),
+            const SizedBox(height: 16),
+            const Row(
+              children: [
+                Text("Price", style: AppFonts.bb2SemiBold),
+                Spacer(),
+                Text("\$87.10", style: AppFonts.bb2SemiBold),
+              ],
+            ),
+            const SizedBox(height: 16),
+            const Row(
+              children: [
+                Text("Squid Sweet and Sour Salad",
+                    style: AppFonts.captionRegular),
+                Spacer(),
+                Text("\$87.10", style: AppFonts.captionRegular),
+              ],
+            ),
+            const SizedBox(height: 8),
+            const Row(
+              children: [
+                Text("Japan Hainanese Sashimi", style: AppFonts.captionRegular),
+                Spacer(),
+                Text("\$87.10", style: AppFonts.captionRegular),
+              ],
+            ),
+            const SizedBox(height: 8),
+            const Row(
+              children: [
+                Text("Black Pepper Beef Lumpia",
+                    style: AppFonts.captionRegular),
+                Spacer(),
+                Text("\$87.10", style: AppFonts.captionRegular),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Container(
+                width: double.infinity,
+                height: 1,
+                color: const Color(0xFFEAEBED),
+              ),
+            ),
+            const Row(
+              children: [
+                Text("Shipping", style: AppFonts.bb2SemiBold),
+                Spacer(),
+                Text("\$87.10", style: AppFonts.bb2SemiBold),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Container(
+                width: double.infinity,
+                height: 1,
+                color: const Color(0xFFEAEBED),
+              ),
+            ),
+            const Row(
+              children: [
+                Text("Total Payment", style: AppFonts.bb2SemiBold),
+                Spacer(),
+                Text("\$87.10", style: AppFonts.bb2SemiBold),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
