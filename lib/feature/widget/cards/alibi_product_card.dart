b9316092ec30e1a7ca4c287c624edfb89e74e3f0
tursunalikhorunaliyev <khorunaliyev@gmail.com>
2024-02-09 21:28:30 +0500

    bug in flutter page controller

--- lib/feature/widget/cards/alibi_product_card.dart
+++ lib/feature/widget/cards/alibi_product_card.dart
@@ -1,5 +1,6 @@
 import 'dart:ui';
 
+import 'package:alibi_shop/feature/shopprod/screen/shop_product_screen.dart';
 import 'package:alibi_shop/generated/assets.dart';
 import 'package:alibi_shop/values/colors.dart';
 import 'package:alibi_shop/values/typography.dart';
@@ -7,6 +8,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 
 class AlibiProductCard extends StatefulWidget {
   final String imageUrl;
@@ -33,92 +35,96 @@ class _AlibiProductCardState extends State<AlibiProductCard> {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRect(
-      child: BackdropFilter(
-        filter: ImageFilter.blur(
-          sigmaX: 0,
-          sigmaY: 17,
-        ),
-        child: Container(
-          width: 164.w,
-          height: widget.cardHeight,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(12),
+    return GestureDetector(
+      onTap: () => context.push(ShopProductScreen.routeName),
+      child: ClipRect(
+        child: BackdropFilter(
+          filter: ImageFilter.blur(
+            sigmaX: 0,
+            sigmaY: 17,
           ),
-          child: Column(
-            children: [
-              SizedBox(
-                width: double.infinity,
-                height: widget.imageHeight,
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(12),
-                    topRight: Radius.circular(12),
-                  ),
-                  child: CachedNetworkImage(
-                    imageUrl: widget.imageUrl,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              Expanded(
-                  child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 12,
-                  vertical: 8,
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    const Text(
-                      "NIKE Run Swift 2",
-                      style: AppFonts.bb2Medium,
+          child: Container(
+            width: 164.w,
+            height: widget.cardHeight,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  height: widget.imageHeight,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      topRight: Radius.circular(12),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "670,000 UZS",
-                              style: AppFonts.overlineRegular
-                                  .copyWith(color: Color(0xFFC4C5C9)),
-                            ),
-                            Text(
-                              "520,000 UZS",
-                              style: AppFonts.captionSemiBold.copyWith(
-                                color: const Color(0xFF614FE0),
+                    child: CachedNetworkImage(
+                      imageUrl: widget.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 8,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Text(
+                        "NIKE Run Swift 2",
+                        style: AppFonts.bb2Medium,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "670,000 UZS",
+                                style: AppFonts.overlineRegular
+                                    .copyWith(color: Color(0xFFC4C5C9)),
                               ),
-                            ),
-                          ],
-                        ),
-                        Material(
-                          color: Colors.transparent,
-                          child: InkWell(
-                            borderRadius: BorderRadius.circular(8),
-                            onTap: () => widget.onClick(widgetKey),
-                            child: Container(
-                              padding: const EdgeInsets.all(8),
-                              decoration: ShapeDecoration(
-                                color: AppColors.licorice,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8),
+                              Text(
+                                "520,000 UZS",
+                                style: AppFonts.captionSemiBold.copyWith(
+                                  color: const Color(0xFF614FE0),
                                 ),
                               ),
-                              child: SvgPicture.asset(Assets.iconsBagOutlined),
-                            ),
+                            ],
                           ),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
-              ))
-            ],
+                          Material(
+                            color: Colors.transparent,
+                            child: InkWell(
+                              borderRadius: BorderRadius.circular(8),
+                              onTap: () => widget.onClick(widgetKey),
+                              child: Container(
+                                padding: const EdgeInsets.all(8),
+                                decoration: ShapeDecoration(
+                                  color: AppColors.licorice,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                ),
+                                child:
+                                    SvgPicture.asset(Assets.iconsBagOutlined),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ))
+              ],
+            ),
           ),
         ),
       ),
